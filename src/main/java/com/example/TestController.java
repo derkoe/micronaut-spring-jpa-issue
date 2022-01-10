c9b785d936767c1e8ed72c16b49731ac7c31202f
Christian Köberl <christian.koeberl@porscheinformatik.com>
2022-01-10 11:05:52 +0100

    Use Micronaut parent POM

--- src/main/java/com/example/TestController.java
+++ src/main/java/com/example/TestController.java
@@ -1,13 +1,13 @@
 package com.example;
 
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+  @Autowired
   private final TestEntityRepository entityRepository;
   
   public TestController(TestEntityRepository entityRepository) {
@@ -15,7 +15,7 @@ public class TestController {
   }
 
   @GetMapping
-  public List<TestEntity> wow() {
+  public Iterable<TestEntity> wow() {
     return entityRepository.findAll();
   }
 }
